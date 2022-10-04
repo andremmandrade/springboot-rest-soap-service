40222249051e1c911dc2e02aa2071c74fcd0935d
André Mesquita Marques de Andrade <andremarquesandrade@gmail.com>
2022-10-04 19:33:37 -0300

    Update ControllerRS.java

--- initial/src/main/java/com/example/restservice/ControllerRS.java
+++ initial/src/main/java/com/example/restservice/ControllerRS.java
@@ -17,7 +17,7 @@ import io.spring.guides.gs_web_service.Country;
 @RestController
 public class ControllerRS {
 
-	private static final String template = "Ok, %s!";
+	private static final String template = "DevOps4Women ready, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	private CountryRepository countryRepository;
 
